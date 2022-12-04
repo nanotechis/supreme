74c4bcd4ef649eec7f9083a282cf68b76e21654e
nanotechis <119031742+nanotechis@users.noreply.github.com>
2022-12-04 20:15:13 +0800

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+MYIP=$(curl -sS ipv4.icanhazip.com)
+echo "Checking VPS"
+#########################
+IZIN=$(curl -sS https://raw.githubusercontent.com/nanotechis/supreme/aio/permission/ip | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo -e "\e[32mPermission Accepted...\e[0m"
+else
+echo -e "\e[31mPermission Denied!\e[0m";
+exit 0
+fi
 #########################
 clear
 cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
